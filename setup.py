f0a3b603a2537bbcc22bd37b47e643ef9ce82b75
Courtney Hodge <yss2zv@virginia.edu>
2024-07-02 11:00:30 -0400

    adding setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "Test",
+    version = '0.1',
+    description = 'A simple demo package'.
+    url = '',
+    author = 'Courtney Hodge'
+    author_email = 'yss2zv@virginia.edu',
+    license = 'MIT',
+    packages = ['mypackage']
+
+)
