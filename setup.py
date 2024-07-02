f07e980a1e7cc5d6427dad2ca4208fc6b74d889a
Courtney Hodge <yss2zv@virginia.edu>
2024-07-02 11:14:55 -0400

    adding stuff

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name = "Test",
     version = '0.1',
-    description = 'A simple demo package'.
-    url = '',
-    author = 'Courtney Hodge'
+    description = 'A simple demo package',
+    url = 'https://github.com/courtneyhodge/M09_Excercise',
+    author = 'Courtney Hodge',
     author_email = 'yss2zv@virginia.edu',
     license = 'MIT',
     packages = ['mypackage']
